b79a7244a79815a2326a55800d90a0b1816a26dd
Soumya Avasthi <94033087+soumyaavasthi26@users.noreply.github.com>
2025-05-05 22:47:32 +0530

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
 
     int bt[n], wt[n], tat[n];
-    wt[0] = 0;
+    float total_wt = 0, total_tat = 0;
 
     cout << "Enter burst times:\n";
     for (int i = 0; i < n; i++) {
@@ -15,15 +15,27 @@ int main() {
         cin >> bt[i];
     }
 
+    wt[0] = 0;
+
+    // Calculate waiting time
     for (int i = 1; i < n; i++) {
         wt[i] = wt[i - 1] + bt[i - 1];
     }
 
-    cout << "\nProcess\tBT\tWT\tTAT\n";
+    // Calculate turnaround time
     for (int i = 0; i < n; i++) {
         tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    cout << "\nProcess\tBT\tWT\tTAT\n";
+    for (int i = 0; i < n; i++) {
         cout << "P" << i + 1 << "\t" << bt[i] << "\t" << wt[i] << "\t" << tat[i] << endl;
     }
+
+    cout << "\nAverage Waiting Time: " << total_wt / n;
+    cout << "\nAverage Turnaround Time: " << total_tat / n << endl;
 
     return 0;
 }
